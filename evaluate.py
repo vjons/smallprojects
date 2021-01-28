79ce1090bf5048d304a2577a83e9835778bbd228
vjons <vjonskth@gmail.com>
2021-01-28 11:18:03 +0100

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,31 @@
 import operator as op
 import numpy as np
+from sympy import symbols,lambdify
+from sympy.core.expr import Expr
 
 tuplefy=lambda x:x if isinstance(x,tuple) else (x,)
 
-def evaluate(it,opers,prs,p_rpr=0):
+def to_number(expr):
+    x=expr.replace('.','',1)
+    if x.isdigit():
+        return float(expr)
+    elif x[:-1].isdigit():
+        return complex(expr) 
+    else:
+        return False
+
+def to_func(res):
+    return lambdify(res.free_symbols,res)
+
+def evaluate(it,opers,prs,p_rpr=0,**variables):
     if not p_rpr:
-        prs.update({" ":(0,0),"(":(0,1),")":(1,0),",":(1.1,1.2),"\n":(1,0)})
+        prs.update({" ":(0,0),"(":(0,1),")":(1,0),",":(1.5,1.6),"\n":(1,0)})
+        prs.update({k:(0,100) for k in set(opers)-set(prs) if callable(opers[k])})
     it=iter(it)
     operator=None
     operands=()
     will={}
+    kw={}
     pr="("
     while pr!=")":
         expr=""
@@ -18,7 +34,8 @@ def evaluate(it,opers,prs,p_rpr=0):
             continue
 
         if expr and not expr in prs:
-            operands+=tuplefy(opers.get(expr) or eval(expr))
+            value=variables.get(expr) or opers.get(expr) or to_number(expr) or symbols(expr)
+            operands+=tuplefy(value)
 
         if expr in prs and any(prs[expr]): op=expr
         elif any(prs[ch]): op=ch
@@ -28,17 +45,17 @@ def evaluate(it,opers,prs,p_rpr=0):
 
         if ch=="(":
             pr=ch
-            if operands: op="*"
+            if operands and "*" in opers: op="*"
         else: pr=op
         lpr,rpr=prs[pr]
         operator=opers.get(op,lambda x:x)
         if 0<lpr<=p_rpr: break
         if rpr:
-            right,ch=evaluate(it,opers,prs,rpr)
+            right,ch=evaluate(it,opers,prs,rpr,**variables)
             will["it"]=rpr==1
             operands+=tuplefy(right)
-        
-        operands=operator(*operands),
+
+        operands=operator(*operands,**kw),
     return operands+(op,) if p_rpr else operands[0]
 
 if __name__=="__main__":
@@ -52,12 +69,13 @@ if __name__=="__main__":
           "+_":op.pos,"+":op.add,"-_":op.neg,"-":op.sub,
           "*":op.mul,"/":op.truediv,"^":op.pow,"add":op.add,
           "sin":np.sin,"cos":np.cos,"ln":np.log,"t_func":t_func,
-          "pi":np.pi,"e":np.e}
+          "pi":np.pi,"e":np.e,"exp":np.exp}
 
     precedence={"[":(0,1),"]":(1,0),"!":(16,0),
                 "+_":(0,12),"+":(4,5),"-_":(0,13),"-":(6,7),
-                "*":(8,9),"/":(10,11),"^":(15,14),"add":(4,5),
-                "sin":(0,16),"cos":(0,16),"ln":(0,16),"t_func":(0,16)}
+                "*":(8,9),"/":(10,11),"^":(15,14),"add":(4,5)}
+
+    #operators that are not constants and appear in operators are added to precedence with a value of (0,100)
 
     expr="+1.23+((+5.23*(1+2j)*4 add 4* cos(4)*t_func(cos(3),2+1*sin(1.5*pi),e^2^-1)+e ^3 +7.26+2j*6.54/ln( 3) -1.9/0.1-2)*(3^ 4-2 )* (((4+3)))* 1.6  - 7)-2.13^-1.34 ^-1*3"
     result=evaluate(expr,operators,precedence)
@@ -66,13 +84,15 @@ if __name__=="__main__":
 
     #Small example using different precedences but where functions are highest in precedence, 0 in left precedence means that it only take right operands
     #operators of length 1 can not be inside named functions or constants!
-    expr="3+4*2+5+func 4 + constant"
-    opers={"*":op.mul,"+":op.add,"func": lambda x:x**2,"constant":20}
+    expr="3+4*2+5+func 4 + c+x"#x if a variable
+    opers={"*":op.mul,"+":op.add,"func": lambda x:x**2,"c":20}
 
-    print(f"mul before add: {expr} = ",evaluate(expr,opers,prs={"+":(2,3),"*":(4,5),"func":(0,6)}))
+    print(f"mul before add: {expr} = ",evaluate(expr,opers,x=4,prs={"+":(2,3),"*":(4,5)}))
 
-    print(f"add before mul: {expr} = ",evaluate(expr,opers,prs={"*":(2,3),"+":(4,5),"func":(0,6)}))
+    print(f"add before mul: {expr} = ",evaluate(expr,opers,x=5,prs={"*":(2,3),"+":(4,5)}))
 
-    print(f"left before right: {expr} = ",evaluate(expr,opers,prs={"*":(2,3),"+":(2,3),"func":(0,6)}))
+    print(f"left before right: {expr} = ",evaluate(expr,opers,x=6,prs={"*":(2,3),"+":(2,3)}))
 
-    print(f"right before left: {expr} = ",evaluate(expr,opers,prs={"*":(3,2),"+":(3,2),"func":(0,6)}))
+    print(f"right before left: {expr} = ",evaluate(expr,opers,prs={"*":(3,2),"+":(3,2)}))
+
+    #In the last example you get a function by using the to_func method which can be evaluated fast later
